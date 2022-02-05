2316d7ea08be6d623c45841c427ccb8b4dfce839
Narendra Singh <narendra315@gmail.com>
2022-02-05 09:31:36 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Loader from './Loader';
 
 function App() {
+  const [showLoader, setShowLoader] = useState(false);
+
+  const onShowLoader = () => {
+    setShowLoader(true);
+    setTimeout(() => {
+      setShowLoader(false);
+    }, 5 * 1000)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 text-center">
+      {
+        showLoader && <Loader />
+      }
+      <button className="btn btn-primary" onClick={() => onShowLoader()}>
+        Show Loader
+      </button>
     </div>
   );
 }
